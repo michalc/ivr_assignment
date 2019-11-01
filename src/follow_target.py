d8ae3e5772b94b069d7b9b9c2abf6ff5bb4afde7
Michal Charemza <michal@charemza.name>
2019-11-01 20:10:27 +0000

    Combined callback for multiple cameras

--- src/follow_target.py
+++ src/follow_target.py
@@ -4,6 +4,7 @@ import logging
 import sys
 
 import cv2
+import message_filters
 import rospy
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
@@ -19,14 +20,14 @@ def main():
   handler.setLevel(logging.INFO)
   logger.addHandler(handler)
 
-  def camera1_callback(_):
-    logger.info('/camera1/robot/image_raw callback')
+  def camera_callback(_, __):
+    logger.info('camera callback')
 
-  def camera2_callback(_):
-    logger.info('/camera2/robot/image_raw callback')
-
-  rospy.Subscriber('/camera1/robot/image_raw', Image, camera1_callback)
-  rospy.Subscriber('/camera2/robot/image_raw', Image, camera2_callback)
+  camera_1_sub = message_filters.Subscriber('/camera1/robot/image_raw', Image)
+  camera_2_sub = message_filters.Subscriber('/camera2/robot/image_raw', Image)
+  message_filters \
+    .ApproximateTimeSynchronizer([camera_1_sub, camera_2_sub], queue_size=1, slop=0.01) \
+    .registerCallback(camera_callback)
 
   try:
     rospy.spin()
