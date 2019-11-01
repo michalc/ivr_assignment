aded77eacd4dbf3035777e0f6f83b407605824ba
Michal Charemza <michal@charemza.name>
2019-11-01 20:15:42 +0000

    Extract image data from cameras inside callback

--- src/follow_target.py
+++ src/follow_target.py
@@ -20,8 +20,13 @@ def main():
   handler.setLevel(logging.INFO)
   logger.addHandler(handler)
 
-  def camera_callback(_, __):
-    logger.info('camera callback')
+  def camera_callback(data_1, data_2):
+    # Allow exceptions to bubble up: they are logged automatically
+    image_1 = bridge.imgmsg_to_cv2(data_1, 'bgr8')
+    image_2 = bridge.imgmsg_to_cv2(data_1, 'bgr8')
+
+    logger.info('Image 1 %s', image_1.shape)
+    logger.info('Image 2 %s', image_2.shape)
 
   camera_1_sub = message_filters.Subscriber('/camera1/robot/image_raw', Image)
   camera_2_sub = message_filters.Subscriber('/camera2/robot/image_raw', Image)
