93c6ae252bf13ea54c758104c41a8e2eb7ae1056
Michal Charemza <michal@charemza.name>
2019-11-01 19:42:30 +0000

    Subscriber to camera 1

---
+++ src/follow_target.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import logging
+import sys
+
+import cv2
+import rospy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+
+def main():
+  rospy.init_node('follow_target', anonymous=True)
+  bridge = CvBridge()
+
+  logger = logging.getLogger()
+  logger.setLevel(logging.INFO)
+  handler = logging.StreamHandler(sys.stdout)
+  handler.setLevel(logging.INFO)
+  logger.addHandler(handler)
+
+  def camera1_callback(_):
+    logger.info('/camera1/robot/image_raw callback')
+
+  rospy.Subscriber('/camera1/robot/image_raw', Image, camera1_callback)
+
+  try:
+    rospy.spin()
+  except KeyboardInterrupt:
+    logger.info('Shutting down')
+  cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
