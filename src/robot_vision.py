2790a5ebd1f31eb3a2a8897a0ce90b06ae16b187
Michal Charemza <michal@charemza.name>
2019-11-03 08:30:04 +0000

    Refactor so image analysis can be called from another node

--- src/robot_vision.py
+++ src/robot_vision.py
@@ -12,26 +12,18 @@ from std_msgs.msg import Float64MultiArray
 from cv_bridge import CvBridge, CvBridgeError
 
 
-def main():
-  rospy.init_node('robot_vision', anonymous=True)
-  circ_pub = rospy.Publisher("orange_circ_center", Float64MultiArray, queue_size=10)
-  bridge = CvBridge()
+colour_ranges = {
+  # Ranges handle the slight shadow towards the bottom of the objects
+  'yellow': ((0, 100, 100), (50, 255, 255)),
+  'blue': ((100, 0, 0), (255, 50, 50)),
+  'green': ((0, 100, 0), (50, 255, 50)),
+  'red': ((0, 0, 100), (50, 50, 255)),
+  'orange': ((75, 110, 130), (95, 175, 220)),
+}
+dilate_kernel = np.ones((5, 5), np.uint8)
 
-  logger = logging.getLogger()
-  logger.setLevel(logging.INFO)
-  handler = logging.StreamHandler(sys.stdout)
-  handler.setLevel(logging.INFO)
-  logger.addHandler(handler)
 
-  colour_ranges = {
-    # Ranges handle the slight shadow towards the bottom of the objects
-    'yellow': ((0, 100, 100), (50, 255, 255)),
-    'blue': ((100, 0, 0), (255, 50, 50)),
-    'green': ((0, 100, 0), (50, 255, 50)),
-    'red': ((0, 0, 100), (50, 50, 255)),
-    'orange': ((75, 110, 130), (95, 175, 220)),
-  }
-  dilate_kernel = np.ones((5, 5), np.uint8)
+def calc_positions_and_angles(image_1, image_2):
 
   def calc_center_of_masses(image, range_names):
     def calc_center_of_mass(colour_range):
@@ -100,64 +92,85 @@ def main():
       None if x_z is None or y_z is None else \
       np.array([x_z[0], y_z[0], (y_z[1] + y_z[1]) / 2])
 
+  # Find joint positions
+  joint_range_names = ('yellow', 'blue', 'green', 'red')
+  joint_centers_1 = calc_center_of_masses(image_1, joint_range_names)
+  joint_centers_2 = calc_center_of_masses(image_2, joint_range_names)
+
+  # Find orange object positions
+  orange_circ_center_1, orange_rect_center_1 = calc_circ_rect_centers(image_1, ('orange',))['orange']
+  orange_circ_center_2, orange_rect_center_2 = calc_circ_rect_centers(image_2, ('orange',))['orange']
+
+  # Use blue and yellow to convert from pixels to meters, since we know/assume they can't be
+  # obscured, know the meter distance between them, and they can't move
+  # We create conversions for both cameras, in case they are different
+  meters_per_pixel_1 = 2.0 / (joint_centers_1['yellow'][1] - joint_centers_1['blue'][1])
+  meters_per_pixel_2 = 2.0 / (joint_centers_2['yellow'][1] - joint_centers_2['blue'][1])
+
+  # Convert all pixel coordinates to their meter coordinates, based on origin 1.0m below the
+  # yellow circle
+  pixel_coords_to_meters_1 = pixel_coords_to_meters_converter(
+    meters_per_pixel_1, joint_centers_1['yellow'] + np.array([0, 1.0 / meters_per_pixel_1])
+  )
+  pixel_coords_to_meters_2 = pixel_coords_to_meters_converter(
+    meters_per_pixel_2, joint_centers_2['yellow'] + np.array([0, 1.0 / meters_per_pixel_2])
+  )
+  joint_centers_1 = {
+    range_name: pixel_coords_to_meters_1(coords) for range_name, coords in joint_centers_1.items()
+  }
+  joint_centers_2 = {
+    range_name: pixel_coords_to_meters_2(coords) for range_name, coords in joint_centers_2.items()
+  }
+  orange_circ_center_1 = pixel_coords_to_meters_1(orange_circ_center_1)
+  orange_rect_center_1 = pixel_coords_to_meters_1(orange_rect_center_1)
+  orange_circ_center_2 = pixel_coords_to_meters_2(orange_circ_center_2)
+  orange_rect_center_2 = pixel_coords_to_meters_2(orange_rect_center_2)
+
+  # Combine the 2D coordinates to 3D
+  joint_centers = {
+    range_name: coords_2d_to_3d(joint_centers_1[range_name], joint_centers_2[range_name]) for range_name in joint_range_names
+  }
+  orange_circ_center = coords_2d_to_3d(orange_circ_center_1, orange_circ_center_2)
+  orange_rect_center = coords_2d_to_3d(orange_rect_center_1, orange_rect_center_2)
+
+  green_to_red = joint_centers['red'] - joint_centers['green']
+  blue_to_green = joint_centers['green'] - joint_centers['blue']
+  link_4 = np.arccos(np.dot(green_to_red, blue_to_green)/(np.linalg.norm(green_to_red) * np.linalg.norm(blue_to_green)))
+
+  return {
+    'joint_centers': joint_centers,
+    'orange_circ_center': orange_circ_center,
+    'orange_rect_center': orange_rect_center,
+    'link_1': None,
+    'link_2': None,
+    'link_3': None,
+    'link_4': link_4
+  }
+
+
+def main():
+  rospy.init_node('robot_vision', anonymous=True)
+  circ_pub = rospy.Publisher("orange_circ_center", Float64MultiArray, queue_size=10)
+  bridge = CvBridge()
+
+  logger = logging.getLogger()
+  logger.setLevel(logging.INFO)
+  handler = logging.StreamHandler(sys.stdout)
+  handler.setLevel(logging.INFO)
+  logger.addHandler(handler)
+
   def camera_callback(data_1, data_2):
     # Allow exceptions to bubble up: they are logged automatically, and will
     # stop the rest of the callback running
     image_1 = bridge.imgmsg_to_cv2(data_1, 'bgr8')
     image_2 = bridge.imgmsg_to_cv2(data_2, 'bgr8')
 
-    # Find joint positions
-    joint_range_names = ('yellow', 'blue', 'green', 'red')
-    joint_centers_1 = calc_center_of_masses(image_1, joint_range_names)
-    joint_centers_2 = calc_center_of_masses(image_2, joint_range_names)
+    positions_and_angles = calc_positions_and_angles(image_1, image_2)
 
-    # Find orange object positions
-    orange_circ_center_1, orange_rect_center_1 = calc_circ_rect_centers(image_1, ('orange',))['orange']
-    orange_circ_center_2, orange_rect_center_2 = calc_circ_rect_centers(image_2, ('orange',))['orange']
+    logger.info('positions_and_angles: %s', positions_and_angles)
 
-    # Use blue and yellow to convert from pixels to meters, since we know/assume they can't be
-    # obscured, know the meter distance between them, and they can't move
-    # We create conversions for both cameras, in case they are different
-    meters_per_pixel_1 = 2.0 / (joint_centers_1['yellow'][1] - joint_centers_1['blue'][1])
-    meters_per_pixel_2 = 2.0 / (joint_centers_2['yellow'][1] - joint_centers_2['blue'][1])
-
-    # Convert all pixel coordinates to their meter coordinates, based on origin 1.0m below the
-    # yellow circle
-    pixel_coords_to_meters_1 = pixel_coords_to_meters_converter(
-      meters_per_pixel_1, joint_centers_1['yellow'] + np.array([0, 1.0 / meters_per_pixel_1])
-    )
-    pixel_coords_to_meters_2 = pixel_coords_to_meters_converter(
-      meters_per_pixel_2, joint_centers_2['yellow'] + np.array([0, 1.0 / meters_per_pixel_2])
-    )
-    joint_centers_1 = {
-      range_name: pixel_coords_to_meters_1(coords) for range_name, coords in joint_centers_1.items()
-    }
-    joint_centers_2 = {
-      range_name: pixel_coords_to_meters_2(coords) for range_name, coords in joint_centers_2.items()
-    }
-    orange_circ_center_1 = pixel_coords_to_meters_1(orange_circ_center_1)
-    orange_rect_center_1 = pixel_coords_to_meters_1(orange_rect_center_1)
-    orange_circ_center_2 = pixel_coords_to_meters_2(orange_circ_center_2)
-    orange_rect_center_2 = pixel_coords_to_meters_2(orange_rect_center_2)
-
-    # Combine the 2D coordinates to 3D
-    joint_centers = {
-      range_name: coords_2d_to_3d(joint_centers_1[range_name], joint_centers_2[range_name]) for range_name in joint_range_names
-    }
-    orange_circ_center = coords_2d_to_3d(orange_circ_center_1, orange_circ_center_2)
-    orange_rect_center = coords_2d_to_3d(orange_rect_center_1, orange_rect_center_2)
-
-    logger.info('joint_centers: %s', joint_centers)
-    logger.info('orange_circ_center: %s, orange_rect_center: %s', orange_circ_center, orange_rect_center)
-
-    if orange_circ_center is not None:
-      circ_pub.publish(Float64MultiArray(data=orange_circ_center))
-
-    green_to_red = joint_centers['red'] - joint_centers['green']
-    blue_to_green = joint_centers['green'] - joint_centers['blue']
-    link_4 = np.arccos(np.dot(green_to_red, blue_to_green)/(np.linalg.norm(green_to_red) * np.linalg.norm(blue_to_green)))
-
-    logger.info('link_4: %s', link_4)
+    if positions_and_angles['orange_circ_center'] is not None:
+      circ_pub.publish(Float64MultiArray(data=positions_and_angles['orange_circ_center']))
 
   camera_1_sub = message_filters.Subscriber('/camera1/robot/image_raw', Image)
   camera_2_sub = message_filters.Subscriber('/camera2/robot/image_raw', Image)
