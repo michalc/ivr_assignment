d2da9a47318d3e995c85597b780f63371929f9e6
Michal Charemza <michal@charemza.name>
2019-11-11 19:41:59 +0000

    Add target detection publisher

---
+++ src/robot_vision_target_detection.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import traceback
+
+import message_filters
+import numpy as np
+import rospy
+from sensor_msgs.msg import Image
+from std_msgs.msg import Float64, Float64MultiArray
+from cv_bridge import CvBridge
+
+from robot_vision import calc_positions_and_angles
+
+
+def main():
+  rospy.init_node('robot_vision_join_state', anonymous=True)
+  target_pub = rospy.Publisher("target", Float64MultiArray, queue_size=10)
+  bridge = CvBridge()
+
+  def camera_callback(data_1, data_2):
+    try:
+      image_1 = bridge.imgmsg_to_cv2(data_1, 'bgr8')
+      image_2 = bridge.imgmsg_to_cv2(data_2, 'bgr8')
+      target_center = calc_positions_and_angles(image_1, image_2)['target_center']
+      print('target_center', target_center)
+      target_pub.publish(Float64MultiArray(data=target_center))
+    except Exception as ex:
+      traceback.print_exc()
+
+  camera_1_sub = message_filters.Subscriber('/camera1/robot/image_raw', Image)
+  camera_2_sub = message_filters.Subscriber('/camera2/robot/image_raw', Image)
+  message_filters \
+    .ApproximateTimeSynchronizer([camera_1_sub, camera_2_sub], queue_size=1, slop=0.01) \
+    .registerCallback(camera_callback)
+
+  rospy.spin()
+
+if __name__ == '__main__':
+    main()
